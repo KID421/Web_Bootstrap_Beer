f03e112f1d7bdda6716ea37a7f1aa472f657e31d
KID421 <suchild7@gmail.com>
2019-09-19 18:57:48 +0800

    捲動 API 與輸出訊息至控制台

--- js/main.js
+++ js/main.js
@@ -9,4 +9,11 @@ $(document).ready(function () {
         // 找到 html, body 執行動畫 ({動畫屬性: 值}, 時間);
         $('html, body').animate({scrollTop: 0}, 1000);
     });
+
+    // 視窗 - window
+    // 捲動 - scroll()
+    $(window).scroll(function () { 
+        // 輸出訊息至控制台
+        console.log('捲動中~');
+    });
 });
